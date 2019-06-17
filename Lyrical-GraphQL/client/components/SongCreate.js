48d6d2637c57677c75525b1468cb4a86902eb893
Денис Магазейщиков <denismagazeyshchikov@inca-pf0r8wa321.sigma.sbrf.ru>
2019-06-17 16:48:20 +0300

    react router

---
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SongCreate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <div>song create</div>
+  }
+}
+
+export default SongCreate
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-class SongList extends Component {
-  render() {
-    console.log(this.props)
-    return <div>songlist</div>
-  }
-}
-
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => (
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>
+    ))
+  }
+
+  render() {
+    if (this.props.data.loading) {
+      return <span>Loading...</span>
+    }
+    return <ul className="collection">{this.renderSongs()}</ul>
+  }
+}
 
 export default graphql(query)(SongList)
